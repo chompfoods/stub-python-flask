97599b5a24b2ad1758ced1dfe18c04ab3eaf090f
Chomp Food Database API <60017996+chompfoods@users.noreply.github.com>
2021-03-10 07:30:45 -0600

    Add files via upload

--- swagger_server/test/test_default_controller.py
+++ swagger_server/test/test_default_controller.py
@@ -7,6 +7,7 @@ from six import BytesIO
 
 from swagger_server.models.branded_food_object import BrandedFoodObject  # noqa: E501
 from swagger_server.models.ingredient_object import IngredientObject  # noqa: E501
+from swagger_server.models.recipe_object import RecipeObject  # noqa: E501
 from swagger_server.test import BaseTestCase
 
 
@@ -18,7 +19,8 @@ class TestDefaultController(BaseTestCase):
 
         Get a branded food item using a barcode
         """
-        query_string = [('code', 'code_example')]
+        query_string = [('code', 'code_example'),
+                        ('user_id', 'user_id_example')]
         response = self.client.open(
             '/api/v2/food/branded/barcode.php',
             method='GET',
@@ -33,7 +35,8 @@ class TestDefaultController(BaseTestCase):
         """
         query_string = [('name', 'name_example'),
                         ('limit', 56),
-                        ('page', 56)]
+                        ('page', 56),
+                        ('user_id', 'user_id_example')]
         response = self.client.open(
             '/api/v2/food/branded/name.php',
             method='GET',
@@ -59,7 +62,8 @@ class TestDefaultController(BaseTestCase):
                         ('trace', 'trace_example'),
                         ('vitamin', 'vitamin_example'),
                         ('limit', 56),
-                        ('page', 56)]
+                        ('page', 56),
+                        ('user_id', 'user_id_example')]
         response = self.client.open(
             '/api/v2/food/branded/search.php',
             method='GET',
@@ -73,9 +77,75 @@ class TestDefaultController(BaseTestCase):
         Get raw/generic food ingredient item(s)
         """
         query_string = [('find', 'find_example'),
-                        ('limit', 56)]
+                        ('limit', 56),
+                        ('user_id', 'user_id_example')]
         response = self.client.open(
             '/api/v2/food/ingredient/search.php',
+            method='GET',
+            query_string=query_string)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_recipe_id_php_get(self):
+        """Test case for recipe_id_php_get
+
+        Get a recipe by ID
+        """
+        query_string = [('id', 'id_example'),
+                        ('user_id', 'user_id_example')]
+        response = self.client.open(
+            '/api/v2/recipe/id.php',
+            method='GET',
+            query_string=query_string)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_recipe_ingredient_php_get(self):
+        """Test case for recipe_ingredient_php_get
+
+        Get recipes using a list of ingredients
+        """
+        query_string = [('list', 'list_example'),
+                        ('limit', 56),
+                        ('page', 56),
+                        ('user_id', 'user_id_example')]
+        response = self.client.open(
+            '/api/v2/recipe/ingredient.php',
+            method='GET',
+            query_string=query_string)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_recipe_random_php_get(self):
+        """Test case for recipe_random_php_get
+
+        Get random popular recipes
+        """
+        query_string = [('limit', 56),
+                        ('user_id', 'user_id_example')]
+        response = self.client.open(
+            '/api/v2/recipe/random.php',
+            method='GET',
+            query_string=query_string)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_recipe_search_php_get(self):
+        """Test case for recipe_search_php_get
+
+        Get recipes using a title and optional filters
+        """
+        query_string = [('title', 'title_example'),
+                        ('excluded_cuisine', 'excluded_cuisine_example'),
+                        ('included_cuisine', 'included_cuisine_example'),
+                        ('excluded_ingredient', 'excluded_ingredient_example'),
+                        ('included_ingredient', 'included_ingredient_example'),
+                        ('nutrients_required', 56),
+                        ('limit', 56),
+                        ('page', 56),
+                        ('user_id', 'user_id_example')]
+        response = self.client.open(
+            '/api/v2/recipe/search.php',
             method='GET',
             query_string=query_string)
         self.assert200(response,
